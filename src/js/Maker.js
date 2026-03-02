875de2ac143fd61d616c9e40ba33ed3e5f84e790
James Rock <hello@jamesrock.me>
2026-03-02 13:02:05 +0000

    updates

--- src/js/Maker.js
+++ src/js/Maker.js
@@ -1,10 +1,86 @@
 import {
+  DisplayObject,
 	makeArray,
 	makeInput,
 	makeNode,
 	makeButton,
 	makeSelect
 } from '@jamesrock/rockjs';
+import { mazes } from './mazes';
+
+const makeOpacitySlider = () => {
+  const node = makeInput(0, 'range');
+  node.min = 0;
+  node.max = 1;
+  node.step = 0.1;
+  node.value = 1;
+  return node;
+};
+
+class Grid extends DisplayObject {
+  constructor(s, w, h, data = makeArray(w*h, () => 0)) {
+
+    super();
+
+    this.data = data;
+
+    let x = 0;
+    let y = 0;
+    const gap = 1;
+    const node = this.node = makeNode('div', 'grid');
+    node.style.width = `${w*s + (gap * (w-1))}px`;
+    node.style.height = `${h*s + (gap * (h-1))}px`;
+    node.style.gap = `${gap}px`;
+
+    makeArray(w*h).forEach((index) => {
+      const pixel = makeNode('div', 'grid-pixel');
+      pixel.style.width = pixel.style.height = `${s}px`;
+      pixel.dataset.index = index;
+      pixel.dataset.x = x;
+      pixel.dataset.y = y;
+      pixel.dataset.active === 'no';
+      pixel.classList.add(this.guides.includes(x) || this.guides.includes(y) ? 'guide' : 'pixel');
+      node.append(pixel);
+      this.pixels.push(pixel);
+
+      if(x > 0 && x%(w-1)===0) {
+        x = 0;
+        y ++;
+      }
+      else {
+        x ++;
+      };
+
+    });
+
+    this.fill();
+
+  };
+  fill() {
+
+    this.data.forEach((value, index) => {
+      this.pixels[index].dataset.active = this.bobMap[value];
+    });
+
+    return this;
+
+  };
+  set(index, value) {
+
+    this.data[index] = this.activeMap[value];
+    return this;
+
+  };
+  activeMap = {
+    'wall-yes': 1,
+    'wall-no': 0,
+    'coin-yes': 2,
+    'coin-no': 0,
+  };
+  bobMap = ['no', 'yes', 'coin'];
+  guides = makeArray(25).map((value) => value * 3);
+  pixels = [];
+};
 
 export class Maker {
   constructor() {
@@ -42,11 +118,15 @@ export class Maker {
     const inputs = makeNode('div', 'inputs');
     const target = makeNode('div', 'grid-target');
     const difficulty = makeSelect(['easy', 'medium', 'hard'].map((a) => [a, a]));
-    const mode = makeSelect([['add', 'yes'], ['remove', 'no']]);
-    const set = makeSelect([3, 4, 5].map((a) => [a, a]));
+    const mode = makeSelect([['add wall', 'wall-yes'], ['remove wall', 'wall-no'], ['add coin', 'coin-yes'], ['remove coin', 'coin-no']]);
+    const constrain = {
+      'wall-yes': 'guide',
+      'wall-no': 'guide',
+      'coin-yes': 'pixel',
+      'coin-no': 'pixel'
+    };
+    const set = makeSelect(makeArray(10).map((a) => [`#${a + 1}`, a]));
     const copyButton = makeButton('copy', 'copy');
-    const gap = 1;
-    const guides = makeArray(25).map((value) => value * 3);
     let props = settings[difficulty.value];
     const xPos = makeInput(props.xPos);
     const yPos = makeInput(props.yPos);
@@ -54,47 +134,18 @@ export class Maker {
     const pixelSize = makeInput(props.pixelSize);
     const width = makeInput(props.width);
     const height = makeInput(props.height);
-    let data = this.data = [];
-
-    const makeGrid = (s, w, h) => {
-      let x = 0;
-      let y = 0;
-      const out = makeNode('div', 'grid');
-      out.style.width = `${w*s + (gap * (w-1))}px`;
-      out.style.height = `${h*s + (gap * (h-1))}px`;
-      out.style.gap = `${gap}px`;
-      makeArray(w*h).forEach((index) => {
-        const pixel = makeNode('div', 'grid-pixel');
-        pixel.style.width = pixel.style.height = `${s}px`;
-        pixel.dataset.index = index;
-        pixel.dataset.x = x;
-        pixel.dataset.y = y;
-        pixel.dataset.active === 'no';
-        pixel.classList.add(guides.includes(x) || guides.includes(y) ? 'guide' : 'pixel');
-        out.append(pixel);
-
-        if(x > 0 && x%(w-1)===0) {
-          x = 0;
-          y ++;
-        }
-        else {
-          x ++;
-        };
-
-      });
-      return out;
-    };
+    const opacity = makeOpacitySlider();
+    let grid = null;
 
     const changeHandler = () => {
-      data = this.data = makeArray(props.width*props.height, () => 0);
-      body.style.backgroundImage = `url(/mazes/maze-${set.value}-${difficulty.value}.png)`;
+      body.style.backgroundImage = `url(/mazes/maze-${Number(set.value) + 1}-${difficulty.value}.png)`;
       body.style.backgroundSize = `${size.value}px`;
       body.style.backgroundPosition = `calc(50% - ${xPos.value}px) calc(50% - ${yPos.value}px)`;
       if(grid) {
-        grid.parentNode.removeChild(grid);
+        grid.destroy();
       };
-      grid = makeGrid(Number(pixelSize.value), Number(width.value), Number(height.value));
-      target.append(grid);
+      grid = new Grid(Number(pixelSize.value), Number(width.value), Number(height.value), mazes[difficulty.value][set.value]);
+      grid.appendTo(target);
     };
 
     difficulty.addEventListener('input', () => {
@@ -110,9 +161,11 @@ export class Maker {
 
     set.addEventListener('input', changeHandler);
 
-    let grid = null;
+    opacity.addEventListener('input', () => {
+      target.style.opacity = opacity.value;
+    });
 
-    [set, difficulty, mode, copyButton].forEach((input) => {
+    [set, difficulty, mode, copyButton, opacity].forEach((input) => {
       inputs.append(input);
     });
 
@@ -129,40 +182,28 @@ export class Maker {
 
     let knobs = null;
 
-    const activeMap = {
-      'yes': 1,
-      'no': 0
-    };
-
     target.addEventListener('touchstart', (e) => {
       knobs = [];
     });
 
     target.addEventListener('touchmove', (e) => {
      	const knob = document.elementFromPoint(e.touches[0].clientX, e.touches[0].clientY);
-     	if(knob?.classList.contains('guide')) {
+     	if(knob?.classList.contains(constrain[mode.value])) {
     		if(knobs.indexOf(knob)===-1) {
      			knobs.push(knob);
-          // knob.dataset.active = knob.dataset.active === 'yes' ? 'no' : 'yes';
-          knob.dataset.active = mode.value;
-          data[knob.dataset.index] = activeMap[knob.dataset.active];
+          grid.set(knob.dataset.index, mode.value);
     		};
      	};
      	e.preventDefault();
     });
 
     copyButton.addEventListener('click', () => {
-      navigator.clipboard.writeText(JSON.stringify(data));
+      navigator.clipboard.writeText(JSON.stringify(grid.data));
       copyButton.innerText = 'copied!';
       setTimeout(() => {
         copyButton.innerText = 'copy';
       }, 2000);
     });
-
-  };
-  getData() {
-
-    return JSON.stringify(this.data);
 
   };
 };
